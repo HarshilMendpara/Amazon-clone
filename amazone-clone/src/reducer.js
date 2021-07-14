6575b9ae04e9a7933af53c2fff7bc18ccf6c91ce
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-14 21:28:47 +0530

    crerated to store basket data

---
+++ amazone-clone/src/reducer.js
@@ -0,0 +1,17 @@
+export const initialState = {
+    basket: [],
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TO_BASKET':
+            return {
+                ...state,
+                basket: [...state.basket, action.item],
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
