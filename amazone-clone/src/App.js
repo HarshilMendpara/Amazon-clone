29999183f74b771697adece5567f788737929bb8
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-14 21:24:59 +0530

    used react library to navigate between two pages

--- amazone-clone/src/App.js
+++ amazone-clone/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   return (
-    <div className='app'>
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className='app'>
+        <Header />
+        <Switch>
+          <Route path='/checkout'>
+            <Checkout />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
