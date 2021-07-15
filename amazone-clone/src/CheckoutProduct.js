fca4d3826803a5f34cfcb4a009a7ca45a536e20c
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-15 12:36:02 +0530

    created file for checkout products

---
+++ amazone-clone/src/CheckoutProduct.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './CheckoutProduct.css'
+import { useStateValue } from './StateProvider';
+
+function CheckoutProduct({ id, image, title, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+
+    const removeFromBasket = () => {
+        dispatch({
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
+        })
+    }
+
+    return (
+        <div className='checkout_product'>
+            <img className='checkout_product_image' src={image} alt='' />
+
+            <div className='checkout_product_info'>
+
+                <p className='checkout_product_title'>{title}</p>
+
+                <p className='checkout_product_price'>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+
+                <div className='checkout_product_rating'>
+                    {Array(rating).fill().map((_, i) => (
+                        <p> ⭐ </p>
+                    ))}
+                </div>
+
+                <button onClick={removeFromBasket}>Remove from Basket</button>
+
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutProduct
