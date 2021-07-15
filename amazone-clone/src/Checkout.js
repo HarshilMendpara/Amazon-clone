f6bc96bbf37241ad13efe3f06a52c64be23204ac
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-15 12:34:54 +0530

    added basket list at checkout page

--- amazone-clone/src/Checkout.js
+++ amazone-clone/src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Checkout.css'
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider'
 import Subtotal from './Subtotal'
 
 function Checkout() {
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className='checkout'>
             <div className='checkout_left'>
@@ -11,6 +15,17 @@ function Checkout() {
                     <h2 className='checkout_title'>
                         Your Shopping Basket
                     </h2>
+
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                    ))}
+
                 </div>
             </div>
             <div className='checkout_right'>
