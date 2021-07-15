91d88baf8ea7e4f3a68c5cbcdb7995912cbfe4bc
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-15 12:34:05 +0530

    added login route

--- amazone-clone/src/App.js
+++ amazone-clone/src/App.js
@@ -3,17 +3,22 @@ import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Checkout from './Checkout';
+import Login from './Login';
 
 function App() {
   return (
     <Router>
       <div className='app'>
-        <Header />
         <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
           <Route path='/checkout'>
+            <Header />
             <Checkout />
           </Route>
           <Route path='/'>
+            <Header />
             <Home />
           </Route>
         </Switch>
