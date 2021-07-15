bf7e75c246fe931ffdaf3dc0e3c0234de6cb9fb8
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-15 12:37:31 +0530

    added case to remove product from basket

--- amazone-clone/src/reducer.js
+++ amazone-clone/src/reducer.js
@@ -2,6 +2,8 @@ export const initialState = {
     basket: [],
 };
 
+export const getBasketTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
     switch (action.type) {
         case 'ADD_TO_BASKET':
@@ -9,6 +11,24 @@ const reducer = (state, action) => {
                 ...state,
                 basket: [...state.basket, action.item],
             };
+
+        case 'REMOVE_FROM_BASKET':
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Can't remove product (id: ${action.id}) as its not in basket!`
+                )
+            }
+            return {
+                ...state,
+                basket: newBasket
+            }
+
         default:
             return state;
     }
