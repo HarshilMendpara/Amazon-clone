54607fc2c4b61fa2986ab9a1284c70b55f237643
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-16 15:43:11 +0530

    added auth for sign in and register

--- amazone-clone/src/Login.js
+++ amazone-clone/src/Login.js
@@ -1,17 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import React, { useState } from 'react';
 import './Login.css';
+import { auth } from './firebase';
 
 function Login() {
+    const history = useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const signIn = e => {
         e.preventDefault();
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
     }
 
     const register = e => {
         e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                if (auth) {
+                    history.push('/');
+                }
+            })
+            .catch(error => alert(error.message))
     }
 
     return (
