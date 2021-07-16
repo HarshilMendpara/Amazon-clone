cfa8431f30385cd1b73b8ea1a01b3c03329425c2
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-16 15:49:31 +0530

    added username in checkout page

--- amazone-clone/src/Checkout.js
+++ amazone-clone/src/Checkout.js
@@ -5,13 +5,14 @@ import { useStateValue } from './StateProvider'
 import Subtotal from './Subtotal'
 
 function Checkout() {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
 
     return (
         <div className='checkout'>
             <div className='checkout_left'>
                 <img className='checkout_ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' alt='' />
-                <div >
+                <div>
+                    <h3>Hello, {user?.email.substring(0, user.email.lastIndexOf("@"))}</h3>
                     <h2 className='checkout_title'>
                         Your Shopping Basket
                     </h2>
