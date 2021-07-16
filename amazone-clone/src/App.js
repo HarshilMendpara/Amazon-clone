5b0b96ca9f877ec887f906c402cb682cc897830e
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-16 15:42:10 +0530

    added function to set user status

--- amazone-clone/src/App.js
+++ amazone-clone/src/App.js
@@ -4,8 +4,35 @@ import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Checkout from './Checkout';
 import Login from './Login';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+
+    auth.onAuthStateChanged(authUser => {
+
+      console.log(authUser);
+
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }
+      else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+
+    })
+  }, [])
+
   return (
     <Router>
       <div className='app'>
