8c4a3d9a08b4e3686c8f973fb3320373ab46f8ed
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-17 20:48:33 +0530

    linked button with payment page

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -3,8 +3,10 @@ import CurrencyFormat from 'react-currency-format';
 import './Subtotal.css';
 import { useStateValue } from './StateProvider';
 import { getBasketTotal } from './reducer';
+import { useHistory } from 'react-router-dom';
 
 function Subtotal() {
+    const history = useHistory();
     const [{ basket }, dispatch] = useStateValue();
 
     return (
@@ -26,7 +28,7 @@ function Subtotal() {
                 thousandSeparator={true}
                 prefix={"$"}
             />
-            <button>Proceed to Checkout</button>
+            <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
         </div>
     );
 }
