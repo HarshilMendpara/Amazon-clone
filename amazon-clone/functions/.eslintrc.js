31c2eef4dfff167963e9646055ef464549a6c01b
HarshilMendpara <mendparaharshil@gmail.com>
2021-07-18 21:20:33 +0530

    used firebase configs for env variables

--- amazon-clone/functions/.eslintrc.js
+++ amazon-clone/functions/.eslintrc.js
@@ -11,4 +11,5 @@ module.exports = {
   rules: {
     quotes: ["error", "double"],
   },
+  parser: "babel-eslint",
 };
--- amazon-clone/functions/index.js
+++ amazon-clone/functions/index.js
@@ -1,34 +1,32 @@
 const functions = require("firebase-functions");
-const express = require('express');
-const cors = require('cors');
-require("dotenv").config();
-const stripe = require('stripe')(process.env.SECRET_KEY)
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(functions.config().stripe.key);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (request, response) => response.status(200).send("Hello world"));
+app.get("/", (request, response) => response.status(200).send("Hello world"));
 
-app.post('/payment/create', async (req, res) => {
-    const total = req.query.total;
-    try {
+app.post("/payment/create", async (request, response) => {
+  const total = request.query.total;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "inr",
+    });
 
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: total,
-            currency: 'inr',
-        });
-
-        res.status(201).send({
-            clientSecret: paymentIntent.client_secret,
-        })
-    } catch (err) {
-        res.status(500).send({
-            err: err,
-        })
-    }
-})
+    response.status(201).send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (err) {
+    response.status(500).send({
+      err: err,
+    });
+  }
+});
 
 exports.api = functions.https.onRequest(app);
 
